fe6207c00f4198acce41fba52c29104799e3c9bb
Michael Hughes <michaeltimothy@live.ca>
2015-03-08 16:06:01 -0300

    server.js file completed
    scraping code completed.

--- sitescraper/server.js
+++ sitescraper/server.js
@@ -17,10 +17,10 @@ app.get('/scrape', function (req, res) {
      * function takes 3 parameters, an error, response
      * status code and the html
      */
-    request(url, function(error, response, html){
+    request(url, function (error, response, html) {
         // First we'll check to make sure no errors occured when making the request
-        
-        if(!error){
+
+        if (!error) {
             // Next, we'll utilize the cheerio library on the return html
             // which will essentially give us jQuery functionality
 
@@ -49,14 +49,42 @@ app.get('/scrape', function (req, res) {
 
                 release = data.children().last().children().text();
 
-                // Once we have our title, we'll store it to the json object.
+                // Once we have our title and release, we'll store it to the json object.
 
                 json.title = title;
                 json.release = release;
-                
-           })
+
+            })
+
+            // Since the rating is in a different section of the DOM, we'll
+            // have to write a new jQuery filter to extract this information.
+
+            $('.star-box-giga-star').filter(function () {
+                var data = $(this);
+
+                // The .star-box-giga-star class was exactly where we wanted it
+                // to be. To get the rating, we can simply just get the .text()
+                // no need to traverse the DOM any further
+
+                rating = data.text();
+                json.rating = rating;
+            })
         }
-    })
+
+        // To write to the system we will use the built in 'fs' library.
+        // In this example we will pass 3 parameters to the writeFile function
+        // Parameter 1: output.json - this is what the created filename will be called
+        // Parameter 2: JSON.stringify(json, null, 4) - the data to write, here we do
+        // an extra step by calling JSON.stringify to make our JSON easier to read
+        // Parameter 3: callback function - a callback function to let us know the status of our function
+
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err) {
+            console.log('File successfully written! - Check your project directory for the output.json file');
+        })
+
+        // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+        res.send('Check your console')
+    });
 })
 
 app.listen('8081')
